f67d754021a7f88317a5ecec4f0ab953c34e62ef
Ailsa.Fan <54009155+you-sunshine@users.noreply.github.com>
2019-12-02 15:07:33 +0800

    Update Test2.java

--- 自定义函数java题/Test2.java
+++ 自定义函数java题/Test2.java
@@ -1,24 +1,44 @@
-import java.util.Scanner;
+package Test1;
 
-public class Test2 {
-    public static void main(String[] args) {
-        /**
-         * 迭代法求a的平方根：temp=a/temp时，temp为其平方根，
-         两者差小于10的负4次方，<1e-4，即为精确到小数点后四位
-         temp  和 a/temp 分别位于a的平方根的两头，一个大于，一个小于，
-         取两者的平均值进行迭代。
-         */
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        float temp=1;
-        while(Math.abs(temp-a/temp)>1e-4)
-        {
-            temp=(temp+a/temp)/2;
-        }
+import java.math.BigInteger;
 
+public class Test1 {
 
-        System.out.println(temp);
-
-
+    public static BigInteger[][] mat_mul(BigInteger[][] x, BigInteger[][] y) {
+        BigInteger[][] res = new BigInteger[][]{
+                new BigInteger[]{
+                        new BigInteger("0"), new BigInteger("0")
+                },
+                new BigInteger[]{
+                        new BigInteger("0"), new BigInteger("0")
+                }
+        };
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 2; k++)
+                    res[i][j] = res[i][j].add(x[i][k].multiply(y[k][j]));
+        return res;
     }
-}
+
+    public static void mat_pow(int n) {
+        BigInteger[][] res = new BigInteger[2][2];
+        BigInteger[][] c = new BigInteger[2][2];
+        c[0][0] = new BigInteger("1");
+        c[0][1] = new BigInteger("1");
+        c[1][0] = new BigInteger("1");
+        c[1][1] = new BigInteger("0");
+                for (int i = 0; i < 2; i++) res[i][i] = new BigInteger("1");
+        for (int i = 0; i < 2; i++) res[i][1 - i] = new BigInteger("0");
+        while (n > 0) {
+        if ((n & 1) == 1) res = mat_mul(res, c);
+                c = mat_mul(c, c);
+                n = n >> 1;
+                }
+                char[] chars = res[0][1].toString().toCharArray();
+                System.out.println(new String(chars, chars.length - 4, 4));
+                }
+
+public static void main(String[] args) {
+        mat_pow(1000000);
+        }
+        }
